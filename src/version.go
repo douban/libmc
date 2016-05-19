418658391613e4963c56b67dbea1f17d736eaf6b
mckelvin <mckelvin@users.noreply.github.com>
2016-05-19 11:13:05 +0800

    golibmc_test: Fix fragile test case "testQuit"
    The bug is not stable to reproduce on Linux and
    it CANNOT be reproduced on OS X. A possible explanation to this
    phenomenon is:
    
    Sending version command will not increase the `curr_connections` metric,
    and only data I/O commands like Set/Get/Delete commands will do. The
    idea of this commit is to make sure the current connection to
    target memcached server is established and confirmed by the server-end
    (curr_connections + 1).

--- src/version.go
+++ src/version.go
@@ -1,9 +1,9 @@
 package golibmc
 
-const _Version = "v1.0.0"
-const _Author = "PAN, Myautsai"
+const _Version = "v1.0.0-5-gcd1baf5"
+const _Author = "mckelvin"
 const _Email = "mckelvin@users.noreply.github.com"
-const _Date = "Thu Mar 24 12:20:00 2016 +0800"
+const _Date = "Thu May 19 11:13:05 2016 +0800"
 
 // Version of the package
 const Version = _Version
