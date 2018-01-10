c551eb5d2e3922c5c2c0d2e13365ed259791c614
tkm <nakagawa.takumi@dena.jp>
2018-01-10 14:56:02 +0900

    Bump Version 1.1.1

--- src/version.go
+++ src/version.go
@@ -1,6 +1,6 @@
 package golibmc
 
-const _Version = "v1.1.0"
+const _Version = "v1.1.1"
 const _Author = "mckelvin"
 const _Email = "mckelvin@users.noreply.github.com"
 const _Date = "Tue Nov 8 11:22:00 2016 +0800"
