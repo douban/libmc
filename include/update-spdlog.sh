eb90828e992bed17110d981746376f81bc8da92c
MOON_CLJ <lijunli2598@gmail.com>
2018-01-18 18:35:19 +0800

    include spdlog headers, and spdlog is header only

---
+++ include/update-spdlog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+VER="0.16.3"
+URL="https://github.com/gabime/spdlog/archive/v${VER}.tar.gz"
+echo "Downloading $URL"
+curl $URL -L -o /tmp/spdlog.tar.gz
+tar xvf /tmp/spdlog.tar.gz
+rm -rf spdlog
+cp -r spdlog-${VER}/include/spdlog  spdlog
+rm -rf spdlog-${VER}
+echo "Use git status, add all files and commit changes."
