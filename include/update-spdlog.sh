23ecc5361fa27803fc5892547633c7812d2b1536
MOON_CLJ <lijunli2598@gmail.com>
2018-01-18 20:20:52 +0800

    z

--- include/update-spdlog.sh
+++ include/update-spdlog.sh
@@ -8,5 +8,5 @@ tar xvf /tmp/spdlog.tar.gz
 rm -rf spdlog
 cp -r spdlog-${VER}/include/spdlog  spdlog
 rm -rf spdlog-${VER}
-patch -p2 < spdlog.patch
+#patch -p2 < spdlog.patch
 echo "Use git status, add all files and commit changes."
