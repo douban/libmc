79c82523a405a7a152e01574739a2c4ea595173f
MOON_CLJ <lijunli2598@gmail.com>
2018-01-18 21:36:11 +0800

    z

--- include/update-spdlog.sh
+++ include/update-spdlog.sh
@@ -22,5 +22,5 @@ rm spdlog/sinks/ostream_sink.h
 rm spdlog/sinks/syslog_sink.h
 rm spdlog/sinks/wincolor_sink.h
 rm spdlog/sinks/windebug_sink.h
-#patch -p2 < spdlog.patch
+patch -p2 < spdlog.patch
 echo "Use git status, add all files and commit changes."
