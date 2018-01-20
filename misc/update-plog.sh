5bb6e5b63b6f59410c1c2f921902a6cee6d57ea2
MOON_CLJ <lijunli2598@gmail.com>
2018-01-20 12:31:27 +0800

    mv scripts

--- misc/update-plog.sh
+++ misc/update-plog.sh
@@ -4,10 +4,13 @@ VER="1.1.3"
 URL="https://github.com/SergiusTheBest/plog/archive/${VER}.tar.gz"
 echo "Downloading $URL"
 curl $URL -L -o /tmp/plog.tar.gz
+
+cd include
 tar xvf /tmp/plog.tar.gz
 rm -rf plog
 cp -r plog-${VER}/include/plog plog
 rm -rf plog-${VER}
+
 rm -rf plog/Converters/
 rm plog/Appenders/AndroidAppender.h
 rm plog/Appenders/ColorConsoleAppender.h
@@ -18,6 +21,7 @@ rm plog/Formatters/FuncMessageFormatter.h
 rm plog/Formatters/MessageOnlyFormatter.h
 rm plog/Formatters/CsvFormatter.h
 rm plog/WinApi.h
-python rm_plog_win32_code.py
-patch -p2 < plog.patch
+python ../misc/rm_plog_win32_code.py
+patch -p2 < ../misc/plog.patch
+
 echo "Use git status, add all files and commit changes."
