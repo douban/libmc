92fcf07bc9921983a907e1e9fde7edd9058808a4
lihan <tclh123@gmail.com>
2019-01-18 15:51:41 +0800

    add debug_client

---
+++ tests/debug_client.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "Common.h"
+#include "Client.h"
+#include "test_common.h"
+
+using douban::mc::Client;
+using douban::mc::tests::newClient;
+
+
+void set_(Client* client, const char* const* keys, const size_t* key_lens,
+          const flags_t* flags, const char* const* vals, const size_t* val_len, size_t n = 1) {
+
+  const exptime_t exptime = 0;
+  const int noreply = 0;
+
+  message_result_t **m_results = NULL;
+  size_t nResults = 0;
+
+  client->set(keys, key_lens, flags, exptime, NULL, noreply, vals, val_len,
+              n, &m_results, &nResults);
+  client->destroyMessageResult();
+}
+
+
+void get_(Client* client, const char* const * keys, const size_t* key_lens, size_t n = 1) {
+  retrieval_result_t** r_results = NULL;
+  size_t nResults = 0;
+  client->get(keys, key_lens, n, &r_results, &nResults);
+  client->destroyRetrievalResult();
+}
+
+
+int main() {
+	while (true) {
+		Client* client = newClient(20);
+
+		const char* key = "hello";
+		const size_t key_len = 5;
+
+		const char* val = "world";
+		const size_t val_len = 5;
+
+		flags_t flags = 0;
+		set_(client, &key, &key_len, &flags, &val, &val_len, 1);
+
+		// get_(client, &key, &key_len, 1);
+
+		retrieval_result_t** r_results = NULL;
+  		size_t nResults = 0;
+
+  		client->get(&key, &key_len, 1, &r_results, &nResults);
+  		client->destroyRetrievalResult();
+
+		std::cout << r_results[0]->data_block << std::endl;
+
+		delete client;
+	}
+
+	return 0;
+}
