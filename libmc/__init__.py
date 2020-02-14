cd86673a8359f0ff395fd7c7e42e4b404607c64a
everpcpc <git@everpcpc.com>
2020-02-14 12:58:27 +0800

    Bump version 1.3.2
    ChangeLog:
    
    - Fix travis distribute wheel

--- libmc/__init__.py
+++ libmc/__init__.py
@@ -28,10 +28,10 @@ from ._client import (
 )
 
 __VERSION__ = "1.3.0"
-__version__ = "v1.3.1"
+__version__ = "v1.3.2"
 __author__ = "mckelvin"
 __email__ = "mckelvin@users.noreply.github.com"
-__date__ = "Fri Feb 14 12:46:49 2020 +0800"
+__date__ = "Fri Feb 14 12:56:09 2020 +0800"
 
 
 class Client(PyClient):
--- src/version.go
+++ src/version.go
@@ -1,9 +1,9 @@
 package golibmc
 
-const _Version = "v1.3.1"
+const _Version = "v1.3.2"
 const _Author = "mckelvin"
 const _Email = "mckelvin@users.noreply.github.com"
-const _Date = "Fri Feb 14 12:46:49 2020 +0800"
+const _Date = "Fri Feb 14 12:56:09 2020 +0800"
 
 // Version of the package
 const Version = _Version
