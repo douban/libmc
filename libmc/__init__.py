d4190da0c266c2d466b81dec88be78c06bf8029f
everpcpc <everpcpc@users.noreply.github.com>
2021-05-20 18:55:46 +0800

    feat: remove python3.5, add python3.9 (#116)
    * feat: remove python3.5, add python3.9
    
    * Bump version 1.4.2

--- libmc/__init__.py
+++ libmc/__init__.py
@@ -27,11 +27,11 @@ from ._client import (
     __file__ as _libmc_so_file
 )
 
-__VERSION__ = "1.4.1"
-__version__ = "v1.4.1-10-ga2080d1"
+__VERSION__ = "1.4.2"
+__version__ = "v1.4.2"
 __author__ = "mckelvin"
 __email__ = "mckelvin@users.noreply.github.com"
-__date__ = "Thu May 20 18:37:41 2021 +0800"
+__date__ = "Thu May 20 18:44:42 2021 +0800"
 
 
 class Client(PyClient):
--- src/version.go
+++ src/version.go
@@ -1,9 +1,9 @@
 package golibmc
 
-const _Version = "v1.4.1-10-ga2080d1"
+const _Version = "v1.4.2"
 const _Author = "mckelvin"
 const _Email = "mckelvin@users.noreply.github.com"
-const _Date = "Thu May 20 18:37:41 2021 +0800"
+const _Date = "Thu May 20 18:44:42 2021 +0800"
 
 // Version of the package
 const Version = _Version
