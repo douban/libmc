96500cbb1e37b16f879e7bf19fd3b5c8405446fe
Kent Slaney <kent@slaney.org>
2023-11-28 12:30:06 -0800

    moved unix client to test_unix

--- tests/test_unix.cpp
+++ tests/test_unix.cpp
@@ -6,8 +6,14 @@
 #include "gtest/gtest.h"
 
 using douban::mc::Client;
-using douban::mc::tests::newUnixClient;
+using douban::mc::tests::md5Client;
 using douban::mc::splitServerString;
+
+Client* newUnixClient() {
+  const char * hosts[] = { "/tmp/env_mc_dev/var/run/unix_test.socket" };
+  const uint32_t ports[] = { 0 };
+  return md5Client(hosts, ports, 1);
+}
 
 TEST(test_unix, establish_connection) {
   Client* client = newUnixClient();
