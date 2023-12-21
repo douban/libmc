4d7ecdbba1a258069f6d72837c54907862d54aeb
Kent Slaney <kent@slaney.org>
2023-12-21 00:01:10 -0800

    split up tests

--- tests/test_client_pool.py
+++ tests/test_client_pool.py
@@ -10,26 +10,29 @@ class ThreadedSingleServerCase(unittest.TestCase):
 
     def misc(self):
         for i in range(5):
-            with self.pool.client() as mc:
-                tid = str(mc._get_current_thread_ident() + (i,))
-                f, t = 'foo ' + tid, 'tuiche ' + tid
-                mc.get_multi([f, t])
-                mc.delete(f)
-                mc.delete(t)
-                assert mc.get(f) is None
-                assert mc.get(t) is None
-
-                mc.set(f, 'biu')
-                mc.set(t, 'bb')
-                assert mc.get(f) == 'biu'
-                assert mc.get(t) == 'bb'
-                assert (mc.get_multi([f, t]) ==
-                        {f: 'biu', t: 'bb'})
-                mc.set_multi({f: 1024, t: '8964'})
-                assert (mc.get_multi([f, t]) ==
-                        {f: 1024, t: '8964'})
+            self.test_misc()
 
     def test_misc(self):
+        with self.pool.client() as mc:
+            tid = str(mc._get_current_thread_ident() + (i,))
+            f, t = 'foo ' + tid, 'tuiche ' + tid
+            mc.get_multi([f, t])
+            mc.delete(f)
+            mc.delete(t)
+            assert mc.get(f) is None
+            assert mc.get(t) is None
+
+            mc.set(f, 'biu')
+            mc.set(t, 'bb')
+            assert mc.get(f) == 'biu'
+            assert mc.get(t) == 'bb'
+            assert (mc.get_multi([f, t]) ==
+                    {f: 'biu', t: 'bb'})
+            mc.set_multi({f: 1024, t: '8964'})
+            assert (mc.get_multi([f, t]) ==
+                    {f: 1024, t: '8964'})
+
+    def test_misc_threaded(self):
         ts = [Thread(target=self.misc) for i in range(8)]
         for t in ts:
             t.start()
