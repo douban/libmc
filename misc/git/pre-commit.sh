d440b51fb3cb84accb2a8caad945ce8c7657011b
Kent Slaney <kent@slaney.org>
2023-12-26 07:48:38 +0000

    ubuntu compat

--- misc/git/pre-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-DIR=$( cd -- "$( dirname -- `readlink -f "${BASH_SOURCE[0]}"` )" &> /dev/null && pwd )
-HOOK="$DIR/../../.git/hooks/pre-commit"
-
-[[ ! -e "$HOOK" ]] && ln -s "$DIR/pre-commit.sh" "$HOOK"
-git apply --cached "$DIR/debug.patch" -R --recount -q
-exit 0
