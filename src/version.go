a11c0674b0f71e8f89ac45865cca4c614f545f51
Kent Slaney <kent@slaney.org>
2024-06-06 15:28:30 -0700

    version bump

--- src/version.go
+++ src/version.go
@@ -1,9 +1,9 @@
 package golibmc
 
-const _Version = "v1.4.4"
+const _Version = "1.4.5"
 const _Author = "mckelvin"
 const _Email = "mckelvin@users.noreply.github.com"
-const _Date = "Sat Jun  1 05:10:05 2024 +0800"
+const _Date = "Fri Jun  7 06:16:00 2024 +0800"
 
 // Version of the package
 const Version = _Version
